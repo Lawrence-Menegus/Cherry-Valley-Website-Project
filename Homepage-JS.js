72ba7e938200befab93150aebf5c12071abaae32
Kaitlin Diaz <k.diaz1452@gmail.com>
2024-09-03 23:43:40 -0400

    Successful homepage slideshow

--- Homepage-JS.js
+++ Homepage-JS.js
@@ -5,3 +5,21 @@ menu.addEventListener('click', function(){
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 10000); // Change image every 2 seconds
+}
+});
+
