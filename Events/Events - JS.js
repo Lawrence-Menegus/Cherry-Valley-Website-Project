d52f6e57db0434bfc81dafbc565c5a587b810ad7
Larry Menegus <lmenegus7@gmail.com>
2024-09-10 15:48:40 -0400

    4th Commit

--- Events/Events - JS.js
+++ Events/Events - JS.js
@@ -1,3 +1,4 @@
+// For buttons
 const menu = document.querySelector("#mobile-menu");
 const menuLinks = document.querySelector(".navbar__menu");
 
@@ -5,3 +6,28 @@ menu.addEventListener("click", function () {
   menu.classList.toggle("is-active");
   menuLinks.classList.toggle("active");
 });
+
+
+// For Photo Gallery 
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.display = (i === index) ? 'block' : 'none';
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+    showSlide(currentSlide);
+}
+
+// Initial setup
+showSlide(currentSlide);
